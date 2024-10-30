ab4aa683f5cea369d5174777f36b6d5b39893282
alejandro <aortiz-2023a@corhuila.edu.co>
2024-10-30 09:51:04 -0500

    quiz

--- quiz.sql
+++ quiz.sql
@@ -1,9 +1,14 @@
-select 
-*
-from 
-persona p 
-ORDER BY p.nombre
-INNER JOIN role_module rm ON r.id = rm.role_id
-INNER JOIN module m ON rm.module_id = m.id
-INNER JOIN module_view mv ON m.id = mv.module_id
-INNER JOIN view v ON mv.view_id = v.id
+ELECT 
+    r.name AS rol,
+    m.name AS nombre,
+    m.path AS paquete,
+    v.name AS vista,
+    v.path AS ruta
+FROM
+    role r 
+    INNER JOIN role_module rm ON r.id = rm.role_id
+    INNER JOIN module m ON rm.module_id = m.id
+    INNER JOIN module_view mv ON m.id = mv.module_id
+    INNER JOIN view v ON mv.view_id = v.id 
+WHERE r.state = true
+ORDER BY r.name
